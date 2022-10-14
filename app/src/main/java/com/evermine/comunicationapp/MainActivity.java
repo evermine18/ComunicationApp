683faf0d1a2e1308dc95bfc3d7e6ae9af93b8ad1
Edgar <edgarsanou@gmail.com>
2022-10-14 19:33:17 +0200

    Implemented a listview that gets the ip and update the UI with the new info

--- app/src/main/java/com/evermine/comunicationapp/MainActivity.java
+++ app/src/main/java/com/evermine/comunicationapp/MainActivity.java
@@ -3,12 +3,121 @@ package com.evermine.comunicationapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        list = new ArrayList<String>();
+        list.add("test1");
+        list.add("test2");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1,list
+        );
+        ListView lv = (ListView) findViewById(R.id.listView);
+        lv.setAdapter(adapter);
+        Button b1 = (Button) findViewById(R.id.button);
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+        //TextView tv = (TextView) findViewById(R.id.textView);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                executor.execute(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        //Background work here
+                        String result = getDataFromUrl("https://api.myip.com");
+                        try {
+                            JSONObject jObject = new JSONObject(result);
+                            list.add(jObject.getString("ip"));
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+
+
+                        Log.i("ComApp",result);
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                //UI Thread work here
+                                //tv.append(result+"\n");
+                                adapter.notifyDataSetChanged();
+                            }
+                        });
+                    }
+                });
+            }
+        });
+
+    }
+
+    String error = ""; // string field
+    private String getDataFromUrl(String demoIdUrl) {
+
+        String result = null;
+        int resCode;
+        InputStream in;
+        try {
+            URL url = new URL(demoIdUrl);
+            URLConnection urlConn = url.openConnection();
+
+            HttpsURLConnection httpsConn = (HttpsURLConnection) urlConn;
+            httpsConn.setAllowUserInteraction(false);
+            httpsConn.setInstanceFollowRedirects(true);
+            httpsConn.setRequestMethod("GET");
+            httpsConn.connect();
+            resCode = httpsConn.getResponseCode();
+
+            if (resCode == HttpURLConnection.HTTP_OK) {
+                in = httpsConn.getInputStream();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(
+                        in, "iso-8859-1"), 8);
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line).append("\n");
+                }
+                in.close();
+                result = sb.toString();
+
+            } else {
+                error += resCode;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
